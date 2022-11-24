8d834681bac95f563cccb7128487949a64d2bf0a
Mend1s <andremendesskt99@gmail.com>
2022-11-24 10:43:10 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-function tocarSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocarSom(idElementAudio) {
+    const elemento = document.querySelector(idElementAudio).play();
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
-document.querySelector('.tecla_pom').onclick = tocarSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const teclas = listaDeTeclas[contador];
+    const instrumento = teclas.classList[1];
+    const idAudio = `#som_${instrumento}` //template string
+
+    listaDeTeclas[contador].onclick = function () {
+        tocarSom(idAudio);
+    };
+
+    teclas.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space'){
+            teclas.classList.add('ativa');
+        }
+    }
+
+    teclas.onkeyup = function () {
+        teclas.classList.remove('ativa');
+    }
+
+} 
